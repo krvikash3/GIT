a4db0a57119f7095828c42ec9a74356f0d9e00d6
Vikash Kumar <krvikash0912@gmail.com>
2026-07-12 23:50:43 +0530

    remove some command

--- index.js
+++ index.js
@@ -2,10 +2,3 @@ function add(a, b) {
   return a + b;
 }
 add(2, 5);
-//git status
-//ls -a
-//run the first command for tracking files git init
-//now you can see here git status -- Untracked files:
-//(use "git add <file>..." to include in what will be committed)
-//      index.js
-//git add <file Name> -- for adding the files in staging area
