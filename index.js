ffdc22af8a5896e69d2860bd549ce50500ef6022
Vikash Kumar <krvikash0912@gmail.com>
2026-07-14 00:51:39 +0530

    add multiply function and update function calls

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ function sub(a, b) {
   if (!(a > b)) throw new Error("A must be greater than B");
   return a - b;
 }
+
+function multiply(a, b) {
+  return a * b;
+}
 add(2, 5);
 add(5, 8);
 sub(2, 9);
+multiply(10, 20);
