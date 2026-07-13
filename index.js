e919baa8b56ba0fd4c47ec06fd4d600e8c92de50
Vikash Kumar <krvikash0912@gmail.com>
2026-07-14 00:36:44 +0530

    I have just changed feat from feat/v brach

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 function add(a, b) {
   return a + b;
 }
+
+function sub(a, b) {
+  if (!(a > b)) throw new Error("A must be greater than B");
+  return a - b;
+}
 add(2, 5);
 add(5, 8);
+sub(2, 9);
