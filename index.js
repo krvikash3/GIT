4a950667f4c6d65651bb90cf1a0dd85a5e1116f0
Vikash Kumar <krvikash0912@gmail.com>
2026-07-14 01:08:47 +0530

    V/multi (#1)
    * add multiply function and update function calls
    
    * add additional multiply function call
    
    * add multiply function call with a single argument
    
    * add additional multiply function call
    
    * fix: update multiply function to ensure positive arguments and correct function call

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ function sub(a, b) {
   if (!(a > b)) throw new Error("A must be greater than B");
   return a - b;
 }
+
+function multiply(a, b) {
+  if (a < 0 || b < 0) throw new Error("this is a and b is must be postive");
+  return a * b;
+}
 add(2, 5);
 add(5, 8);
 sub(2, 9);
+multiply(10, 20);
+multiply(40, 30);
+multiply(20, 2);
+multiply(40, 40);
